9cd18a1fcb9eb3b6db13776aaf0d8d2de0f9e8e6
sadegh shokhan <sadegh_shokhan@yahoo.com>
2026-09-17 12:31:08 +0330

    create inputUser for manage annual invest

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header.jsx"
+import InputUser from "./components/InputUser.jsx"
 
 function App() {
   return (
     <>
     <Header/>
+    <InputUser/>
     </>
   )
 }
